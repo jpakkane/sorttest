d4e5edf36922c87bd6e929f8d9c4cfd9840b52cc
Jussi Pakkanen <jpakkane@gmail.com>
2017-11-10 22:33:54 +0200

    Add C++ version.

--- cppsort.cpp
+++ cppsort.cpp
@@ -1,4 +1,5 @@
 #include<stdlib.h>
+#include<algorithm>
 
 #define NUM_INTS 100000000
 int numbahs[NUM_INTS];
@@ -10,12 +11,8 @@ void create_random() {
     }
 }
 
-int compare(const void *a , const void *b) {
-    return *((const int*)a) < *((const int*)b);
-}
-
 int main(int argc, char **argv) {
     create_random();
-    qsort(numbahs, NUM_INTS, sizeof(int), compare);
+    std::sort(numbahs, numbahs+NUM_INTS, [](const int a, const int b) { return a < b;});
     return 0;
 }
