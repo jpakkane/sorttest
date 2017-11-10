e94b5e5f9d3cfbfd5c772b62506263383ed92e0f
Jussi Pakkanen <jpakkane@gmail.com>
2017-11-10 20:46:27 +0200

    Compiles and runs.

--- sorter.cpp
+++ sorter.cpp
@@ -1,5 +1,38 @@
 #include<sorter.h>
+#include<string>
+#include<cstdio>
+#include<cstdlib>
+#include<dlfcn.h>
+#include<unistd.h>
 
-extern "C" sorter build_sorter(const char *type_str, const char *comparer) {
-    return nullptr;
+const char *fn_template = R"(
+#include<algorithm>
+
+LESS_FUNC
+
+extern "C" void sort_impl(void *data, TYPENAME size) {
+  TYPENAME * typed_data = reinterpret_cast<TYPENAME*>(data);
+  std::sort(typed_data, typed_data + size, is_less);
+}
+)";
+
+extern "C" sorter build_sorter(const char *type_str, const char *compare_str) {
+    std::string code{fn_template};
+
+    auto func_loc = code.find("LESS_FUNC");
+    code.replace(func_loc, 9, compare_str);
+    auto type_loc = code.find("TYPENAME");
+    while(type_loc != std::string::npos) {
+        code.replace(type_loc, 8, type_str);
+        type_loc = code.find("TYPENAME");
+    }
+    FILE *f = fopen("tmpfile.cpp", "w");
+    fwrite(code.c_str(), code.size(), 1, f);
+    fclose(f);
+    system("c++ -O2 -g -shared -fPIC -o libtemp.so tmpfile.cpp");
+    void *h = dlopen("libtemp.so", RTLD_LAZY);
+    unlink("libtemp.so");
+    unlink("tempfile.cpp");
+    void *ptr = dlsym(h, "sort_impl");
+    return (sorter)ptr;
 }
--- sorttest.c
+++ sorttest.c
@@ -4,7 +4,7 @@
 int numbahs[NUM_INTS];
 
 int main(int argc, char **argv) {
-    sorter x = build_sorter("int", "int less(int a, int b) { return a < b; }");
+    sorter x = build_sorter("int", "int is_less(const int a, const int b) { return a < b; }");
     (*x)(numbahs, NUM_INTS);
     return 0;
 }
